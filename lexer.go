74e102f1c5f357fbd8b120e9746cfd9594ef4667
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-03-04 09:45:59 +0530

    added a few helper func

--- lexer.go
+++ lexer.go
@@ -5,10 +5,30 @@ package main
 // if token = While then start = curr = W and keep moving curr till we finish to token
 // once curr = 'e' add token 'While' and skip start to character after 'e'
 
-type Lexer struct{
+type Lexer struct {
 	source string
 	tokens []Token
-	start int 
-	curr int
-	line int
+	start  int
+	curr   int
+	line   int
+}
+
+// returns whether we have reached the EOF or not
+func (lex *Lexer) isAtEnd() bool {
+	return lex.curr >= len(lex.source)
+}
+
+// add a scanned token
+func (lex *Lexer) addToken(token Token) {
+	lex.tokens = append(lex.tokens, token)
+}
+
+// move the curr pointer
+func (lex *Lexer) advance() {
+	lex.curr++
+}
+
+// check what char is at a given position in the source file
+func (lex *Lexer) charAt(pos int) byte {
+	return lex.source[lex.curr]
 }
