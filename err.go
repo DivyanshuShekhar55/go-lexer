33ad8ddb40f70d789db23be8640430e83af344ed
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-03-04 11:39:03 +0530

    error reporting function

--- err.go
+++ err.go
@@ -1,7 +1,14 @@
-package main 
+package main
+
+import "fmt"
 
 type Err struct {
-	line int
+	line  int
 	token TokenKind
-	msg string
+	msg   string
 }
+
+func (e *Err) reportErr(line int, token TokenKind, msg string) error {
+	return fmt.Errorf("Err at %d : %s \n%s", line, token, msg) 
+}
+
