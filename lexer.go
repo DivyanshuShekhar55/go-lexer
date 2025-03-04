f9231f3564fdc7c28140937a7c80de48ce677de8
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-03-04 10:42:03 +0530

    added first set of scanning tokens

--- lexer.go
+++ lexer.go
@@ -19,7 +19,9 @@ func (lex *Lexer) isAtEnd() bool {
 }
 
 // add a scanned token
-func (lex *Lexer) addToken(token Token) {
+func (lex *Lexer) addToken(tokenType TokenKind, literal interface{}) {
+	text := lex.source[lex.start:lex.curr]
+	token := newToken(tokenType, text, literal, lex.line)
 	lex.tokens = append(lex.tokens, token)
 }
 
@@ -30,5 +32,66 @@ func (lex *Lexer) advance() {
 
 // check what char is at a given position in the source file
 func (lex *Lexer) charAt(pos int) byte {
-	return lex.source[lex.curr]
+	return lex.source[pos]
+}
+
+// we start the tokenisation process from here ...
+func Tokenise(source string) []Token {
+	lex := Lexer{}
+
+	lex.start = 0
+	lex.curr = 0
+	lex.source = source
+	lex.line = 1
+
+	for !lex.isAtEnd() {
+		//lex.advance()
+		lex.start = lex.curr
+		//lex.scanToken()
+
+	}
+
+	// we have reached the end of the file
+	lex.addToken(EOF, nil)
+
+	return lex.tokens
+}
+
+func (lex *Lexer) scanToken() {
+	lex.advance()
+	ch := lex.charAt(lex.curr)
+
+	switch ch {
+	case '(':
+		lex.addToken(LEFT_PAREN, nil)
+		break
+	case ')':
+		lex.addToken(RIGHT_PAREN, nil)
+		break
+	case '{':
+		lex.addToken(LEFT_BRACE, nil)
+		break
+	case '}':
+		lex.addToken(RIGHT_BRACE, nil)
+		break
+	case ',':
+		lex.addToken(COMMA, nil)
+		break
+	case '.':
+		lex.addToken(DOT, nil)
+		break
+	case '-':
+		lex.addToken(MINUS, nil)
+		break
+	case '+':
+		lex.addToken(PLUS, nil)
+		break
+	case ';':
+		lex.addToken(SEMICOLON, nil)
+		break
+	case '*':
+		lex.addToken(STAR, nil)
+		break
+
+	}
 }
